5c9eab3688e9a8512d21d9e585a351eb9bdc069d
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2019-01-31 08:11:52 +0100

    Use optimized MML string

--- src/MathMLRenderer.ts
+++ src/MathMLRenderer.ts
@@ -2,20 +2,22 @@
 
 'use strict';
 
-const mml = require('./MathMLReader');
+const reader = require('./MathMLReader');
 const opMap = require('./operationMap');
 const zlib = require('zlib');
 
 const conf = require('../config.js');
 
-mml.base.prototype.imgUrl = function(format:string|boolean = false) {
+reader.base.prototype.imgUrl = function(format: string | boolean = false) {
   /**
    * @return {string}
    */
-  function toMML(mml:string) {
-    mml =    `<math xmlns="http://www.w3.org/1998/Math/MathML" display="inline">${mml}</math>`;
-
-    return zlib.deflateSync(mml).toString("base64");
+  function toMML(mml: string) {
+    if (!mml.trim().startsWith('<math')) {
+      mml = `<math xmlns="http://www.w3.org/1998/Math/MathML" display="inline">${mml}</math>`;
+    }
+    mml = reader(mml).toMinimalPmml().toString();
+    return zlib.deflateSync(mml).toString('base64');
   }
 
   let node = this.first();
@@ -28,12 +30,12 @@ mml.base.prototype.imgUrl = function(format:string|boolean = false) {
   const collapsedApply = format === 'collapsed' && this.name() === 'apply';
   if (!collapsedApply && opMap.hasOwnProperty(node.name())) {
     node = opMap[node.name()];
-  } else {
+  } else if (node.refNode().length) {
     node = node.refNode();
   }
   const mml = toMML(node.toString());
   return `${conf.get('mathoidUrl')}/zlib/svg/mml/${encodeURIComponent(mml)}`;
 };
 
-module.exports = mml;
+module.exports = reader;
 
